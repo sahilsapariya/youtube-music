84697b1f2fdfb07b0113a96f9b2f9b5ff116e587
sahilsapariya-jeavio <sahil.sapariya@jeavio.com>
2025-03-27 19:37:29 +0530

    feat: theme implementation in progress

--- src/screens/home/Home.tsx
+++ src/screens/home/Home.tsx
@@ -3,7 +3,7 @@ import React from "react";
 
 const Home = () => {
   return (
-    <View>
+    <View style={styles.container}>
       <Text>Home</Text>
     </View>
   );
@@ -11,4 +11,8 @@ const Home = () => {
 
 export default Home;
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  container: {
+
+  }
+});
